79d1f2750e2df02d777abc464edc0d370cbf8f3c
olewintechsolutions <olewintechsolutions@gmail.com>
2024-09-28 19:25:10 +0530

    added new branch

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Hello Welcome</h1>
+        <h2>Successfully integrated CI/CD pipeline</h2>
+   
+        <a href="https://olewintechsolutions.com">olewintechsolutions.com</a>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyNavbar() {
+  return (
+    <div>
+        <h1>Hello Welcome</h1>
+        <h2>Successfully integrated CI/CD pipeline</h2>
+      
+    </div>
+  )
+}
+
+export default MyNavbar;
